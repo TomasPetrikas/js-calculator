080e7cd8bcb6da20bd4551a96309ea8b8cee061d
Tomas Petrikas <tomas.petrikas99@gmail.com>
2022-01-08 15:23:55 +0200

    Add number input

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let displayValue = "";
+
 function add(a, b) {
   return a + b;
 }
@@ -17,3 +19,35 @@ function divide(a, b) {
 function operate(a, op, b) {
   return op(a, b);
 }
+
+function updateDisplay() {
+  const display = document.querySelector("#display");
+  display.textContent = displayValue;
+}
+
+function addDigit(e) {
+  displayValue += e.target.value;
+  updateDisplay();
+}
+
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+
+zero.addEventListener("click", addDigit);
+one.addEventListener("click", addDigit);
+two.addEventListener("click", addDigit);
+three.addEventListener("click", addDigit);
+four.addEventListener("click", addDigit);
+five.addEventListener("click", addDigit);
+six.addEventListener("click", addDigit);
+seven.addEventListener("click", addDigit);
+eight.addEventListener("click", addDigit);
+nine.addEventListener("click", addDigit);
